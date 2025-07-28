be94f6986c30aa579c375d5c33ead227dbf74616
viniciusnora00 <vnora@everco.work>
2025-07-28 20:38:18 -0300

    Primeiro commit sobre react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/Navbar';
+
+
+function App() {
+
+  return (
+    <div className='container-navbar'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
