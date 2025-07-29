c208a07b85f709ce65744eacea4e1e37b371a15a
viniciusnora00 <vnora@everco.work>
2025-07-29 20:12:53 -0300

    Inicio de um header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import './Navbar.css';
+import sgLogo from '../../assets/sg-logo.jpg';
+import iconUser from '../../icons/user.png'
+import shoppingCart from '../../icons/shopping-cart.png'
+import magnifyingGlass from '../../icons/magnifying-glass.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <h1>SG Store</h1>
-    </div>
-  )
-}
 
-export default Navbar
+      <div className='user-area'>
+        <img src={iconUser} alt="User Ícone" />
+        <img src={shoppingCart} alt="Carrinho de Compras" />
+      </div>
+
+      <img src={sgLogo} alt="SG Logo" className='logo'/>
+
+      <div className='navbar-links'>
+        <div className='search-box'>
+          <input type="text" placeholder='Buscar' />
+          <img src={magnifyingGlass} alt="Lupa" />
+        </div>
+      </div>
+
+      {/* Dropdown de Categorias */}
+      <div className="dropdown">
+        <span className="dropdown-title">Categorias</span>
+        <div className="dropdown-content">
+          <div>Body, croppeds & blusas</div>
+          <div>Blusas básicas</div>
+          <div>Calças, shorts & saias</div>
+          <div>Kimonos</div>
+          <div>Vestidos & macacões</div>
+          <div>Tricots & jaquetas</div>
+          <div>T-Shirts</div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
